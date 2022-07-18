10750a2b8574254a5fed6e990ca4272e3043052f
Abdul Sobur Gbadejoko <swaley@Abduls-MacBook-Air.local>
2022-07-18 21:09:13 +0100

    fresh projects

---
+++ da-projects/covid.sql
@@ -0,0 +1,72 @@
+use portfolio_projects;
+select * from coviddeaths 
+limit 10;
+
+select *
+from coviddeaths
+where continent is not null
+order by 3,4;
+
+select *
+from covidvaccinations
+order by 3,4; 
+
+
+select location, date, total_cases, new_cases,total_deaths, population
+from coviddeaths
+order by 1;
+
+-- diving into the total cases vs total deaths
+-- shows likelihood of dying from covid in your country
+select location, date, total_cases,total_deaths, (total_deaths / total_cases)* 100 as Deathpercent
+from coviddeaths
+
+order by 1;
+
+select location, date, total_cases,total_deaths, (total_deaths / total_cases)* 100 as Deathpercent
+from coviddeaths
+where continent is not null
+order by 1;
+
+-- total cases vs population
+-- shows what percentage of population got covid 
+select location, date, population, total_cases , (total_cases / population)* 100 as PercentPopulationInfected
+from coviddeaths
+order by 1;
+
+-- countries with the highest infection rate in comparison to its population
+
+select location, population, MAX(total_cases) as HighestInfectionCount , MAX((total_cases / population))* 100 as percentpopulationinfected
+from coviddeaths
+where continent is not null
+group by 1,2
+order by percentpopulationinfected desc; 
+
+-- showing countries with highest death count per population
+
+select location, MAX(total_deaths) as TotalDeathCount
+from coviddeaths
+-- where location like %africa% 
+where continent is not null 
+group by location
+order by TotalDeathCount desc;
+
+desc coviddeaths;
+
+-- breakdown by continent 
+
+select continent, MAX(total_deaths) as TotalDeathCount
+from coviddeaths
+-- where location like %africa% 
+ where continent is not null 
+group by continent
+order by TotalDeathCount desc;
+
+
+-- global numbers
+
+select location, date, total_cases, total_deaths 
+
+
+
+ 
